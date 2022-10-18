a09566baaf5f53d4441c6c85893563c8e1e8a758
Ilya <ilyaa@Ilyas-MacBook-Air.local>
2022-10-18 21:00:01 +0300

    Just finding out which repo it is

--- script.bash
+++ script.bash
@@ -1,66 +1,64 @@
-#!/bin/env bash
-export LANG=en_US.UTF-8
+#!/bin/bash
+export LANG="en_US.UTF-8"
 
 function dir() {
     name="${1##*/}"
-    if [[ -d "$1" ]]; then
-        ans1=$(($ans1+1))
-    elif [[ -f "$1" ]]; then
-        ans2=$(($ans2+1))
+    if [[ -d $1 ]]; then
+        ans1=$((ans1+1))
+    elif [[ -f $1 ]]; then
+        ans2=$((ans2+1))
     fi
     printf "$3$4$name\n"
-    local subd=("${1}"/*)
-    if [[ -d "${subd[0]}" ]] || [[ -f "${subd[0]}" ]]; then
+    local subd=(${1}/*)
+    if [[ -d ${subd[0]} ]] || [[ -f ${subd[0]} ]]; then
         true
     else
         return
     fi
     local count=$2
     local i=0
-    local f="$3"
-    local size="${#subd[@]}"
-    local arg5="$5"
+    local f=$3
+    local size=${#subd[@]}
     for (( ; i<$size; i++ ))
     {
-        local child="${subd[$i]}"
-        if [ "$arg5" == 0 ];
+        if [ ${5} == 0 ];
         then
             l="$f\u0020\u0020\u0020\u0020"
         else
             l="$f\u2502\u00A0\u00A0\u0020"
         fi
-        if [ $(($i+1)) == $size ];
+        if [ $((i+1)) == ${#subd[@]} ];
         then
             r="\u2514\u2500\u2500\u0020"
-            dir "$child" "$((1+count))" "$l" "$r" 0
+            dir ${subd[i]} $((1+count)) $l $r 0
         else
             r="\u251c\u2500\u2500\u0020"
-            dir "$child" "$((1+count))" "$l" "$r" 1
+            dir ${subd[i]} $((1+count)) $l $r 1
         fi
     }
 }
 
-
-cd dir
-if [ -z "$1" ]; then
+if [ -z $1 ]; then
     root="."
 else
-    root="$1"
+    root=$1
 fi
-printf "$root\n"
-root_subd=("${root}"/*)
+if [ -d $root ]; then
+    printf "$root\n"
+else
+    printf "$root\u0020\u0020[error opening dir]\n"
+fi
+root_subd=(${root}/*)
 ans1=0
 ans2=0
-sz=${#root_subd[@]}
 if [[ -d ${root_subd[0]} ]] || [[ -f ${root_subd[0]} ]]; then
-    for (( j=0; j<$sz; j++ ))
+    for (( i=0; i<${#root_subd[@]}; i++ ))
     do
-        child="${root_subd[$j]}"
-        if [ $(($j+1)) == $sz ];
+        if [ $((i+1)) == ${#root_subd[@]} ];
         then
-            dir "$child" 1 "" "\u2514\u2500\u2500\u0020" 0
+            dir ${root_subd[i]} 1 "" "\u2514\u2500\u2500\u0020" 0
         else
-            dir "$child" 1 "" "\u251c\u2500\u2500\u0020" 1
+            dir ${root_subd[i]} 1 "" "\u251c\u2500\u2500\u0020" 1
         fi
     done
 fi
@@ -72,5 +70,5 @@ fi
 if [ $ans2 == 1 ]; then
     files="file"
 fi
-echo ""
-echo "$ans1 $dirs, $ans2 $files"
+echo
+echo $ans1 "$dirs", $ans2 "$files"
